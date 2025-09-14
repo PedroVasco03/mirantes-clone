7056a002387be44b28d1436bb8a52821c1ab0bfe
Pedro Vasco <baltazarpedro092@gmail.com>
2025-09-14 13:36:03 +0100

    updating

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,13 +1,13 @@
 // src/routes.tsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home/Home";
-import Auth from "./pages/auth/Auth";
-import Company from "./pages/company/Company";
-import Courses from "./pages/courses/Courses";
-import Faq from "./pages/faq/Faq";
-import Pricing from "./pages/pricing/Pricing";
-import Job from "./pages/jobs/Job";
-import Profile from "./pages/profile/Profile";
+import Home from "../pages/home/Home";
+import Auth from "../pages/auth/Auth";
+import Company from "../pages/company/Company";
+import Courses from "../pages/courses/Courses";
+import Faq from "../pages/faq/Faq";
+import Pricing from "../pages/pricing/Pricing";
+import Job from "../pages/jobs/Job";
+import Profile from "../pages/profile/Profile";
 
 export default function AppRoutes() {
   return (

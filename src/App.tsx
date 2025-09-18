ff6e777cc1070b5bbfa9a64726fb2e4ba2edb6a5
Pedro Vasco <baltazarpedro092@gmail.com>
2025-09-18 21:15:32 +0100

    instaling Vercel Web Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
 import './App.css';
 import Home from './pages/home/Home';
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <div className="App">
       <Home/>
       <SpeedInsights />
+       <Analytics />
     </div>
     
   );

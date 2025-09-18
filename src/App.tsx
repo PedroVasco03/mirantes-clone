2c83a02dca24ae9b8d75e8ccc78fa925fc6c7429
Pedro Vasco <baltazarpedro092@gmail.com>
2025-09-18 20:41:55 +0100

    Vercel Speed Insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/home/Home';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function App() {
   return (
     <div className="App">
       <Home/>
+      <SpeedInsights />
     </div>
     
   );
